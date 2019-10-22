aa0bbf70062d37fffa5e2fcd252359d19048964a
Jbwilliams84 <jbwilliams484@gmail.com>
2019-10-22 15:46:24 -0500

    created collection page for each individual page

--- Complete_React_Developer_2019/crwn-clothing/src/firebase/firebase.utils.js
+++ Complete_React_Developer_2019/crwn-clothing/src/firebase/firebase.utils.js
@@ -1,18 +1,16 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-import { createDeflate } from "zlib";
-import { log } from "handlebars";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyB57RrawR6cqIQ2D4Wor22wFuwh4gE_Hh4",
-  authDomain: "crwn-db-89186.firebaseapp.com",
-  databaseURL: "https://crwn-db-89186.firebaseio.com",
-  projectId: "crwn-db-89186",
-  storageBucket: "crwn-db-89186.appspot.com",
-  messagingSenderId: "924635871069",
-  appId: "1:924635871069:web:e2935d6d94bdd0ae2d5ab4",
-  measurementId: "G-764K3ZVWM1"
+  apiKey: 'AIzaSyB57RrawR6cqIQ2D4Wor22wFuwh4gE_Hh4',
+  authDomain: 'crwn-db-89186.firebaseapp.com',
+  databaseURL: 'https://crwn-db-89186.firebaseio.com',
+  projectId: 'crwn-db-89186',
+  storageBucket: 'crwn-db-89186.appspot.com',
+  messagingSenderId: '924635871069',
+  appId: '1:924635871069:web:e2935d6d94bdd0ae2d5ab4',
+  measurementId: 'G-764K3ZVWM1'
 };
 
 //create user information in the database
@@ -36,7 +34,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData
       });
     } catch (error) {
-      console.log("error creating user", error.message);
+      console.log('error creating user', error.message);
     }
   }
 
@@ -49,7 +47,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
--- Complete_React_Developer_2019/crwn-clothing/src/redux/root-reducer.js
+++ Complete_React_Developer_2019/crwn-clothing/src/redux/root-reducer.js
@@ -1,26 +1,28 @@
 // actual code that binds all the states together
 
-import { combineReducers } from "redux";
-import { persistReducer } from "redux-persist";
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 
 // this tells persist you want to use local storage for storage
-import storage from "redux-persist/lib/storage";
+import storage from 'redux-persist/lib/storage';
 
-import userReducer from "./user/user.reducer";
-import cartReducer from "./cart/cart.reducer";
-import directoryReducer from "./directory/directory.reducer";
+import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
 const persistConfig = {
-  key: "root",
+  key: 'root',
   storage,
-  whitelist: ["cart"]
+  whitelist: ['cart']
   //   the item in the array is saying which reducer we want to persist
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
-  directory: directoryReducer
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
