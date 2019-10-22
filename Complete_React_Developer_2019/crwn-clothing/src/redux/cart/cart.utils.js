80959057dd37126906a0307fbe3152fe80792385
Jbwilliams84 <jbwilliams484@gmail.com>
2019-10-22 11:45:00 -0500

    updated directory reducer and selectors

--- Complete_React_Developer_2019/crwn-clothing/src/redux/cart/cart.utils.js
+++ Complete_React_Developer_2019/crwn-clothing/src/redux/cart/cart.utils.js
@@ -14,3 +14,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  // if the quantity is equal to 1 and we decrease it, then we remove it from the cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+  //if the item is more than 1, and we click on the decrease arrow, then this is to decrease by 1
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
--- Complete_React_Developer_2019/crwn-clothing/src/redux/root-reducer.js
+++ Complete_React_Developer_2019/crwn-clothing/src/redux/root-reducer.js
@@ -1,11 +1,26 @@
 // actual code that binds all the states together
 
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+
+// this tells persist you want to use local storage for storage
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+  //   the item in the array is saying which reducer we want to persist
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
